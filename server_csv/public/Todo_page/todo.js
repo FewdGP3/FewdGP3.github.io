52644186292fd4c874dd6b9eb9054a22d416d0ff
kbawto <kbawto@gmail.com>
2021-08-19 16:13:23 +0800

    add comment

--- server_csv/public/Todo_page/todo.js
+++ server_csv/public/Todo_page/todo.js
@@ -65,12 +65,15 @@ document.addEventListener("DOMContentLoaded", function () {
     initialView: "dayGridMonth",
     selectable: true,
     dateClick: function (info) {
-        // info.dateStr 係
+        // info.dateStr 係你click的日期
       console.log("clicked " + info.dateStr);
+      // items 係你click的日期有邊d items
       let items = itemList.filter((item) => item.duedate === info.dateStr);
       let html = "";
+      // 清空另一日的item
       const notCompleted = document.querySelector(".notCompleted");
       notCompleted.innerHTML = "";
+      // display item 出 網站
       console.log(items);
       for (let item of items) {
         addItem(item.name);
