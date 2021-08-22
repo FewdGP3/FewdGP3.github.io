df4e632c6d8ec68c27def4693886d36edf6979ae
ashlee1008 <lee.polam@hotmail.com>
2021-08-23 01:05:42 +0800

    added js html css

--- server_csv/public/Description_page/AddonDescription.js
+++ server_csv/public/Description_page/AddonDescription.js
@@ -12,7 +12,7 @@ const projectList = [
 ]
 
 const mediaList = document.querySelector('.mediaList');
-for (let project of projectList){
+for (let project of projectList){  //project 每一個既object, 每次loop都會refresh 新既值
 console.log(project.projectName)  
 console.log(project.description)
 console.log(project.dueDate)
@@ -59,13 +59,61 @@ addonDescriptionCancelBtn.addEventListener('click', (event) => {
 //     addonDescriptionPopup.classList.add('hide');
 // })
 
+
 addonDescriptionPopup.addEventListener('click', (e) => {
     e.stopPropagation();
-    console.log(123)
+    
 })
 document.querySelector('main').addEventListener('click', (event) => {
     console.log('main')
     addonDescriptionPopup.classList.remove('hide')
 })
+
+addonDescriptionConfirmBtn.addEventListener('click',()=>{
+
+console.log(123)
+    const pjName = document.querySelector('#floatingTextarea3').value
+    console.log(pjName)
+    const dName = document.querySelector('#floatingTextarea4').value
+    console.log(dName)
+    const dueDates = document.querySelector('#addonDate').value
+    console.log(dueDates)
+    const pj = {
+        projectName:pjName.trim(),
+        description:dName.trim(),
+        dueDate:dueDates,
+    }
+    console.log(pj)
+    projectList.push(pj)
+    console.log(projectList)
+    mediaList.innerHTML=''
+    for (let project of projectList){  //project 每一個既object, 每次loop都會refresh 新既值
+        console.log(project.projectName)  
+        console.log(project.description)
+        console.log(project.dueDate)
+        // const projectName = project.projectName 
+        mediaList.innerHTML +=  `
+        <div class="media">
+        <div class="h-status">
+        <i class="fas fa-check-circle"></i>
+        </div>
+        <div class="media-body">
+        <h5 class="mt-0">${project.projectName}</h5>
+        <p>${project.description}</p> 
+        <p>${project.dueDate}</p>
+        </div>
+        <div class="assigned-to">
+        <i class="fas fa-user-circle"></i>
+        <i class="fas fa-user-circle"></i>
+        </div>
+        </div>
+    `
+    }
+
+})
+
+// const newMedia = document.querySelector('.project-control'+'.description-control'+'.assign-to'+'.due-date')
+// newMedia.innerHTML = media.value;
+
 
 // const newAddonUserProject = `<p>'‘Task’' + ${userDescription.value}</p>`
