bb9f18ac0cbdba596f2ee6354e5cf8714cbb8bc4
ashlee1008 <lee.polam@hotmail.com>
2021-09-07 01:33:30 +0800

    react-pj

--- Description-React/react-server/src/pages/Home/components/Item.js
+++ Description-React/react-server/src/pages/Home/components/Item.js
@@ -4,22 +4,27 @@ const Item = ({ id, note, descrip, date, img, deleteData, submittingStatus }) =>
 
   function deleteItem() {
     submittingStatus.current = true
-    deleteData(function(prev) {
+    deleteData(function (prev) {
       return prev.filter(item => item.id !== id)
     })
   }
 
   return (
+    <div className="item-main">
     <div className="item">
       <div>
         <p>PROJECT:{note}</p>
         <p>DESCRIPTION:{descrip}</p>
         <p>DUE DATE:{date}</p>
         <div>
-          ASSIGN TO<img src={png} alt="abc"></img><img src={png} alt="abc"></img><img src={png} alt="abc"></img>
+          ASSIGN TO
+          <img src={png} alt="abc"></img>
+          <img src={png} alt="abc"></img>
+          <img src={png} alt="abc"></img>
         </div>
       </div>
       <button onClick={deleteItem} className="remove">DELETE</button>
+    </div>
     </div>
   );
 };
